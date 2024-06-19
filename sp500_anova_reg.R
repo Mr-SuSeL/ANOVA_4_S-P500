1825e1c187078898efbebfd3dc5c3e91c271ade1
Łukasz Susłowicz <susel.invest@gmail.com>
2024-06-19 12:40:27 +0200

    loading dataset from kaggle and little cleaning

--- sp500_anova_reg.R
+++ sp500_anova_reg.R
@@ -3,3 +3,19 @@ library(dplyr)
 library(ggplot2) 
 # install.packages("plotly")
 library(plotly)
+
+
+# Floating decimal points = 3
+options(digits = 3)
+
+# Load data.
+stocks <- read.csv(
+  file = "financials.csv",
+  header = TRUE,
+  stringsAsFactors = TRUE # To sector analysis.
+)
+
+head(stocks, n = 5)
+stocks <- subset(stocks, select = -SEC.Filings) # to remove col with links
+
+
