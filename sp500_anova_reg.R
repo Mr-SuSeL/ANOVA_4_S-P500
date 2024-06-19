2f36e32109f07705b954447d45bba8d2eaeab967
Łukasz Susłowicz <susel.invest@gmail.com>
2024-06-19 12:30:35 +0200

    libraries loading

---
+++ sp500_anova_reg.R
@@ -0,0 +1,5 @@
+# Data source link: https://www.kaggle.com/datasets/paytonfisher/sp-500-companies-with-financial-information/data?select=financials.csv
+library(dplyr) 
+library(ggplot2) 
+# install.packages("plotly")
+library(plotly)
